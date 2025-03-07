3e7607c5b59bd2713d9f482e03cb53a6a415c5fc
Diego <58009401+marzo245@users.noreply.github.com>
2025-03-07 09:40:59 -0500

    terminando

--- src/main/java/escuelaing/edu/co/EnterpriseArchitectureWorkshop/EnterpriseArchitectureWorkshopApplication.java
+++ src/main/java/escuelaing/edu/co/EnterpriseArchitectureWorkshop/EnterpriseArchitectureWorkshopApplication.java
@@ -1,13 +1,17 @@
 package escuelaing.edu.co.EnterpriseArchitectureWorkshop;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EnterpriseArchitectureWorkshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnterpriseArchitectureWorkshopApplication.class, args);
+	public static void main(String[] args) { 
+		SpringApplication app = new SpringApplication(EnterpriseArchitectureWorkshopApplication.class); 
+		app.setDefaultProperties(Collections.singletonMap("server.port", "5000")); 
+		app.run(args); 
 	}
 
 }
