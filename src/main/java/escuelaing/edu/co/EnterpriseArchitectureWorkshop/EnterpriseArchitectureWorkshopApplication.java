70e5a265401d25e4331d071254c692ddc745a062
Diego <58009401+marzo245@users.noreply.github.com>
2025-03-07 07:52:28 -0500

    feature starting

---
+++ src/main/java/escuelaing/edu/co/EnterpriseArchitectureWorkshop/EnterpriseArchitectureWorkshopApplication.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.co.EnterpriseArchitectureWorkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseArchitectureWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseArchitectureWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/escuelaing/edu/co/EnterpriseArchitectureWorkshop/EnterpriseArchitectureWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.co.EnterpriseArchitectureWorkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseArchitectureWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
